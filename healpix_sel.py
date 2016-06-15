acc3265e276578264e33049efdf7175770057d7c
Kiefer Hicks <khicks@nuodb.com>
2016-06-15 14:39:31 -0400

    Support for scipy sparse matricies.

--- healpix_sel.py
+++ healpix_sel.py
@@ -10,11 +10,12 @@ import time
 
 from healpy.pixelfunc import *
 import healpy
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
 
 import scipy
 import scipy.misc
+import scipy.sparse
 
 def pix_circle(nside, ipix, radius, nest=False):
     assert False
@@ -38,7 +39,7 @@ def draw_circles():
     ax = fig.add_subplot(1,1,1)
     nside = 64
     npix = nside2npix(nside)
-    xmap = np.zeros(npix)
+    xmap = numpy.zeros(npix)
     for i in range(16, 0, -1):
         highlight = pix_circle(nside, 0, i, nest=False)
         for h in highlight:
@@ -75,7 +76,7 @@ def cast_kernel(order, nest=False):
     nside = order2nside(order)
     def kernel(pt1, pt2):
         assert nest
-        dist = 1.0 - np.dot(pix2vec(nside, pt1, nest=nest),
+        dist = 1.0 - numpy.dot(pix2vec(nside, pt1, nest=nest),
                             pix2vec(nside, pt2, nest=nest))
         dist *= math.pi
         return math.exp(-dist * 30)
@@ -94,36 +95,49 @@ def circle(ipix, neighborsof, radius):
         unvisited |= visited
     return visited
 
-def pts2cov(points, kernel):
-    cov = np.zeros((len(points),) * 2)
+def pts2cov(points, kernel, sparse=True):
+    if sparse:
+        cov = scipy.sparse.dok_matrix((len(points),) * 2, numpy.float64)
+    else:
+        cov = numpy.zeros((len(points),) * 2)
     for i, pt1 in enumerate(points):
         for j, pt2 in enumerate(points[i:]):
             variance = kernel(pt1, pt2)
-            cov[i][i + j] = variance
-            cov[j + i][i] = variance
-        cov[i][i] = kernel(pt1, pt1)
+            if math.log10(abs(variance)) > -6:
+                cov[i, i + j] = variance
+                cov[j + i, i] = variance
+        cov[i, i] = kernel(pt1, pt1)
+    if sparse:
+        cov = cov.tocsc()
     return cov
 
 def pts2invcov(points, kernel, neighborsof):
     points = set(points)
-    global_invcov = dict()
+    size = len(points)
+    global_invcov = scipy.sparse.dok_matrix((size, size), numpy.float64)
+    #global_invcov = dict()
     while points:
         center = points.pop()
         region = circle(center, neighborsof, 10)
         hotspot = circle(center, neighborsof, 8)
         points -= hotspot
         region = list(region)
-        cov = pts2cov(region, kernel)
-        local_invcov = np.linalg.inv(cov)
+        cov = pts2cov(region, kernel, sparse=False)
+        local_invcov = numpy.linalg.inv(cov)
         for i, pt1 in enumerate(region):
             for j, pt2 in enumerate(region[i:]):
                 if set((pt1, pt2)) <= hotspot:
-                    global_invcov[frozenset((pt1, pt2))] = local_invcov[i][j + i]
+                    val = local_invcov[i][j + i]
+                    if math.log10(abs(val)) > -6:
+                        #global_invcov[frozenset((pt1, pt2))] =
+                        global_invcov[pt1, pt2] = val
+                        global_invcov[pt2, pt1] = val
+    global_invcov = global_invcov.tocsr()
     return global_invcov
 
 def dict2mat(invcov, order):
     size = nside2npix(order2nside(order))
-    mat = np.zeros((size, size))
+    mat = numpy.zeros((size, size))
     for ent in invcov.keys():
         if len(ent) > 1:
             i, j = ent
@@ -133,25 +147,34 @@ def dict2mat(invcov, order):
         mat[j][i] = invcov[ent]
     return mat
 
+def ss_matmul(csr, csc, size):
+    outmat = numpy.zeros((size,) * 2)
+    for i in range(size):
+        for j in range(size):
+            row = csr.getrow(i).todense()
+            col = csc.getcol(j).todense()
+            outmat[i, j] = numpy.dot(row, col)
+        print(i)
+    return outmat
+
 def main():
-    order = 4
-    starttime = time.time()
-    d = pts2invcov(
-        pointsof(order),
-        cast_kernel(order, nest=True),
-        cast_neighborsof(order, nest=True))
-    tottime = time.time() - starttime
-    savefile = open('order%ddict.pickle' % order, 'wb')
-    print(tottime)
-    pickle.dump(d, savefile, -1)
-    print(len(d))
-    return -1
-    invcov = dict2mat(d, order)
-    cov = pts2cov(pointsof(order), cast_kernel(order, nest=True))
-    check_mat = np.dot(cov, invcov)
+    order = 3
+    pts = pointsof(order)
+    kern = cast_kernel(order, nest=True)
+    neighbor_func = cast_neighborsof(order, nest=True)
+    invcov = pts2invcov(pts, kern, neighbor_func)
+    #for v in di.values():
+    #    print(v)
+    print('invcov_done')
+    cov = pts2cov(pts, kern)
+    print('cov_done')
+    check = ss_matmul(invcov, cov, len(pts))
+    scipy.misc.imsave('check.png', check)
+
+def run_checks():
     scipy.misc.imsave('cov.png', cov)
     scipy.misc.imsave('check.png', check_mat)
-    scipy.misc.imsave('slow_invcov.png', np.linalg.inv(cov))
+    scipy.misc.imsave('slow_invcov.png', numpy.linalg.inv(cov))
     scipy.misc.imsave('invcov.png', invcov)
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
